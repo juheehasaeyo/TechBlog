af0a324c2250ab439e3b53153fd4cb207f4fee6e
조주희 <carla_cho@naver.com>
2024-07-16 20:49:06 +0900

    init: 블로그 초기 세팅 후 배포 테스트

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,16 +1,17 @@
 export const USER_INFORMATIONS = {
-  headerTitle: "EXAMPLE",
-  userName: "EXAMPLE",
-  mascot: "👀",
-  resume: "https://www.EXAMPLE.com",
-  github: "https://www.EXAMPLE.com",
-  domain: "https://www.EXAMPLE.com",
-  meta: {
-    title: "EXAMPLE",
-    description: "EXAMPLE",
-  },
+  "headerTitle": "juheehasaeyo",
+  "userName": "조주희",
+  "mascot": "🌱",
+  "resume": "",
+  "github": "https://github.com/juheehasaeyo",
+  "domain": "https://blog.juheehasaeyo.info",
+  "meta": {
+    "title": "쭈히의 개발 블로그",
+    "description": "프론트엔드 개발자 조주희의 개발 블로그입니다"
+  }
 };
 
 export const CATEGORY_ICONS: Record<string, string> = {
+  DEV: "🖥",
   All: "🏠",
 };
---
+++ src/utils/scripts/overwrite_script.mjs
@@ -0,0 +1,29 @@
+import fs from "fs";
+import path from "path";
+
+const filePath = path.join(process.cwd(), "src/constants/data.ts");
+const fileContents = fs.readFileSync(filePath, 'utf8');
+
+const newUserInfo = {
+  headerTitle: "juheehasaeyo",
+  userName: "조주희",
+  mascot: "🌱",
+  resume: "",
+  github: "https://github.com/juheehasaeyo",
+  domain: "https://blog.juheehasaeyo.info",
+  meta: {
+    title: "쭈히의 개발 블로그",
+    description: "프론트엔드 개발자 조주희의 개발 블로그입니다",
+  },
+};
+
+const newUserInfoJson = JSON.stringify(newUserInfo, null, 2);
+const updatedContents = fileContents.replace(
+  /export const USER_INFORMATIONS = {[\s\S]*?};/,
+  `export const USER_INFORMATIONS = ${newUserInfoJson};`,
+);
+fs.writeFileSync(filePath, updatedContents, "utf8");
+
+console.log("USER_INFORMATIONS 객체가 업데이트되었습니다.");
+console.log("optional 항목 중 작성하지 않은 항목이 있다면 꼭!! 추후 작성해 주세요😉");
+console.log("src/constants/data.ts");
