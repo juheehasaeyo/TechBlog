34f5cd70beb63e062a64657e8344ab8d485fd12d
조주희 <carla_cho@naver.com>
2024-07-16 21:10:59 +0900

    test: 썸네일 이미지 수정 후 테스트

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,17 +1,17 @@
 export const USER_INFORMATIONS = {
-  "headerTitle": "juheehasaeyo",
-  "userName": "조주희",
-  "mascot": "🌱",
-  "resume": "",
-  "github": "https://github.com/juheehasaeyo",
-  "domain": "https://blog.juheehasaeyo.info",
-  "meta": {
-    "title": "쭈히의 개발 블로그",
-    "description": "프론트엔드 개발자 조주희의 개발 블로그입니다"
-  }
+  headerTitle: "juheehasaeyo",
+  userName: "조주희",
+  mascot: "🌱",
+  resume: "",
+  github: "https://github.com/juheehasaeyo",
+  domain: "https://blog.juheehasaeyo.info",
+  meta: {
+    title: "쭈히의 개발 블로그",
+    description: "프론트엔드 개발자 조주희의 개발 블로그입니다",
+  },
 };
 
 export const CATEGORY_ICONS: Record<string, string> = {
-  DEV: "🖥",
+  Dev: "🖥",
   All: "🏠",
 };
