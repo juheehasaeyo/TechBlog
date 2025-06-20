92ca4e6750855d819b98b1b98537ec5965ecfc6f
조주희 <carla_cho@naver.com>
2025-06-20 22:34:32 +0900

    style: 위아래 여백 추가

--- src/components/PostComments.tsx
+++ src/components/PostComments.tsx
@@ -4,17 +4,19 @@ import Giscus from "@giscus/react";
 
 export function PostComments() {
   return (
-    <Giscus
-      repo="juheehasaeyo/TechBlog"
-      repoId="R_kgDOMWKrJg"
-      category="Announcements"
-      categoryId="DIC_kwDOMWKrJs4Crw5x"
-      mapping="pathname"
-      reactionsEnabled="1"
-      emitMetadata="0"
-      inputPosition="top"
-      theme="preferred_color_scheme"
-      lang="ko"
-    />
+    <div className="py-24">
+      <Giscus
+        repo="juheehasaeyo/TechBlog"
+        repoId="R_kgDOMWKrJg"
+        category="Announcements"
+        categoryId="DIC_kwDOMWKrJs4Crw5x"
+        mapping="pathname"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="bottom"
+        theme="preferred_color_scheme"
+        lang="ko"
+      />
+    </div>
   );
 }
